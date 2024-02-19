cd21f99ebf01139c01f5a9cbd65bc8d908fb9231
Ahmedhasan44 <61273031+Ahmedhasan44@users.noreply.github.com>
2024-02-19 19:48:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-// app.js dosyası
+// index.js
+
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = '0.0.0.0'; // Tüm arabirimleri dinle
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Merhaba Dünya!\n');
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Yemek tarifleri</title></head><body><h1>Merhaba Dünya!</h1></body></html>');
 });
 
 server.listen(port, hostname, () => {
